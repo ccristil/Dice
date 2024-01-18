1162dbb1f31d7fedc20cd430892b98c13cf2c5a5
cristiancruz <cristiancruz@CRISTIANCRU9F5C>
2024-01-17 20:33:03 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Dice;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        diceGame dg = new diceGame();
+
+        int games = 0;
+        System.Console.WriteLine("Welcome to the dice throwing simulator!!");
+
+        System.Console.WriteLine("How many dice rolls would you like to simualte?");
+
+        string input = System.Console.ReadLine();
+
+        if (int.TryParse(input, out games))
+        {
+            dg.PlayGame(games);
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+
+    }
+}
---
+++ diceGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    internal class diceGame
+    {
+        private Random random = new Random();
+
+        public void PlayGame(int g) {
+            int die1 = 0;
+            int die2 = 0;
+            int total = 0;
+            string output = "DICE ROLLING SIMULATION RESULTS\r\nEach \"*\" represents 1% of the total number of rolls.\r\nTotal number of rolls = " + g + "\r\n\r\n";
+            string results = "";
+            int[] rollTotals = new int[13];
+
+
+            for (int i = 0; i <= g; i++)
+            {
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+                total = die1 + die2;
+
+                rollTotals[total]++;
+            }
+          
+
+            for (int i  = 2; i <= 12; i++)
+            {
+                string asterisks = "";
+                int percentage = 0;
+                double doubleg = (double)g;
+
+                percentage = (int)(rollTotals[i] / doubleg * 100);
+
+                for (int n = 1; n <= percentage; n++) 
+                {
+                    asterisks += "*";
+                }
+
+                results += i + ": " + asterisks + "\r\n";
+
+            }
+            output += results;
+            output += "\r\nThank you for playing the dice throwing simulator! See ya later!";
+            System.Console.WriteLine(output);
+   
+        }
+    }
+}
